99f198867bdebf4e34dbc9d80320b393636e4377
Akos Szarvak <metalbogyo@gmail.com>
2020-08-25 21:35:52 +0200

    updating navigation css

--- src/components/common/styles.js
+++ src/components/common/styles.js
@@ -1,6 +1,7 @@
 const categoryColors = {
   tech: "rgb(255,59,48)",
   pets: "rgb(0,113,104)",
+  "tech culture": "rgb(0,113,104)",
   "tech news": "rgb(255,149,0)",
   Diamondbacks: "rgb(255,59,48)",
   Suns: "rgb(48,60,163)",
